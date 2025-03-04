54c47c6d655773b087e91be010128cc935f67453
Jymls <35736274+Jymls@users.noreply.github.com>
2025-03-04 14:42:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '臭莹莹',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'og7H07NtTPpUPbyR4iAq70BFfks8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '5_gQ8bPapGZsVMLGZ7Nu7zKy6DOaBFWVL5UCwLbjXs8',
+      useTemplateId: 'IynEOSag-km0wExpswcT81bjz3yKSe8uxUsZUnU9R4M',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-27',
       festivals: [
